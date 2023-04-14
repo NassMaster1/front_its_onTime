399760a457d9a54a217577b18fa412008742023e
nacer <41012118@parisnanterre.fr>
2023-04-14 11:43:47 +0200

    Vzersion error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompaniesComponent} from "./pages/companies/companies.component";
+import {AirportComponent} from "./pages/airport/airport.component";
+import {TimesComponent} from "./pages/times/times.component";
+import {CapacityComponent} from "./pages/capacity/capacity.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:"Companies",component:CompaniesComponent },
+  {path:"airport",component:AirportComponent },
+  {path:"Times",component: TimesComponent},
+  {path:"Capacity",component: CapacityComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
