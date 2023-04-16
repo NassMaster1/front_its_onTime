014dc638faca8ee99ea1a0c34da7107dbe395fe4
nacer <41012118@parisnanterre.fr>
2023-04-16 02:46:32 +0200

    Add feature version 0.1.1

--- src/app/pages/companies/companies.component.ts
+++ src/app/pages/companies/companies.component.ts
@@ -11,22 +11,35 @@ import {CompanieService} from "../../services/companies/companie.service";
 })
 export class CompaniesComponent implements OnInit{
 
+  /*#########################################################################################
+#################################   VARIABLES  ##############################################
+############################################################################################ */
   closeResult!:string;
   errorMessage!:string
   CompanieForm!:FormGroup
   StatesForm!:FormGroup
+  CityForm!:FormGroup
   chart!:Chart
   years!:Array<number>
   codeCompanies!:Array<string>
   listState!:Array<string>
+  listCity!:Array<string>
+
+  /*#########################################################################################
+#############################################################################################
+############################################################################################ */
 
   constructor( private fp: FormBuilder,private modalService: NgbModal,private companieService:CompanieService) { }
 
+  /*#########################################################################################
+#################################   NG ON INIT ##############################################
+############################################################################################ */
   // Ici on initialise le formulaire
   ngOnInit(): void {
     this.inializeyears()
     this.handelGetListCodeCompanies()
     this.handelGetListStates()
+    this.handelGetListcity()
 
     this.CompanieForm=this.fp.group({
       codeCompanie: this.fp.control(null),
@@ -42,6 +55,31 @@ export class CompaniesComponent implements OnInit{
 
     })
 
+    this.CityForm=this.fp.group({
+      codeCityOrigin: this.fp.control(null),
+      codeCityDest:this.fp.control(null),
+      year: this.fp.control(null),
+      typeGraph:this.fp.control(null)
+
+    })
+  }
+
+  /*#########################################################################################
+#############################################################################################
+############################################################################################ */
+
+  /*#########################################################################################
+#################################   GENERAL FUNCTION #########################################
+############################################################################################ */
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   private inializeyears(){
@@ -53,7 +91,14 @@ export class CompaniesComponent implements OnInit{
     this.years=years
   }
 
-  handelGetListCodeCompanies(){
+  /*#########################################################################################
+#############################################################################################
+############################################################################################ */
+
+  /*#########################################################################################
+#################################   APPEL API  ##############################################
+############################################################################################ */
+ handelGetListCodeCompanies(){
     this.companieService.getListCodeCompanies().subscribe(
       {
         next:(data)=>{
@@ -76,16 +121,51 @@ export class CompaniesComponent implements OnInit{
         }
       });
   }
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+
+  handelGetListcity(){
+    this.companieService.getListCity().subscribe(
+      {
+        next:(data)=>{
+          this.listCity=data;
+        },
+        error :(err)=> {
+          this.errorMessage="une erreur serveur s'est produite";
+        }
+      });
   }
 
+  async handleCancelledCompanies() : Promise<any>{
+    let paramGraph=this.CompanieForm.value;
+    let data = await this.companieService.CancelledCompanies(paramGraph["codeCompanie"],paramGraph["year"]).toPromise();
+    return data;
+  }
+
+  async handleDelayStates() : Promise<any>{
+    let paramGraph=this.StatesForm.value;
+    let data = await this.companieService.delayStates(paramGraph["codeStateOrigin"],paramGraph["codeStateDest"],paramGraph["year"]).toPromise();
+    return data;
+  }
+
+  async handleDelayCity() : Promise<any>{
+    let paramGraph=this.CityForm.value;
+    let data = await this.companieService.delayCity(paramGraph["codeCityOrigin"],paramGraph["codeCityDest"],paramGraph["year"]).toPromise();
+    return data;
+  }
+
+  // Fonction qui appelle l'API et retuen un Promise Any pour la syncronisation
+  async handleMeanDelayCompanies() : Promise<any>{
+    let paramGraph=this.CompanieForm.value;
+    let data = await this.companieService.meanDelayCompanies(paramGraph["codeCompanie"],paramGraph["year"]).toPromise();
+    return data;
+  }
+
+  /*#########################################################################################
+#############################################################################################
+############################################################################################ */
+
+  /*#########################################################################################
+#############################   GRAPH FUNCTION AND CONFIGURATION ############################
+############################################################################################ */
   //pour ouvrir la fenetre du formulaire
   opensm(content: any) {
 
@@ -105,13 +185,6 @@ export class CompaniesComponent implements OnInit{
     });
   }
 
- // Fonction qui appelle l'API et retuen un Promise Any pour la syncronisation
-  async handleMeanDelayCompanies() : Promise<any>{
-    let paramGraph=this.CompanieForm.value;
-    let data = await this.companieService.meanDelayCompanies(paramGraph["codeCompanie"],paramGraph["year"]).toPromise();
-    return data;
-  }
-
   //étape 1- La fonction pour appeller notre fonction de l'api
   //étape 2- Appeler la fonction openlg pour ouvrir la fenetre ou afficher le graph
   //étape 33-Appeler la fonction "createChartMeanDelayCompanie" pour creer le graph Chart.js en param: nos données récuperer depuis la fonction "handleMeanDelayCompanies"
@@ -123,6 +196,36 @@ export class CompaniesComponent implements OnInit{
     //étape 3
     let text="Retard des avions pour la companie "+this.CompanieForm.value["codeCompanie"] + " pour l\'anée: "+this.CompanieForm.value["year"]
     this.createCharCompanie(data["mean_delay"],text,this.CompanieForm.value["typeGraph"])
+  }
+
+  async  openCancelledbyyCompanies(content: any) {
+    //étape 1
+    let data = await this.handleCancelledCompanies();
+    //étape 2
+    this.openlg(content)
+    //étape 3
+    let text="Vols annulés pour la companie "+this.CompanieForm.value["codeCompanie"] + " pour l\'anée: "+this.CompanieForm.value["year"]
+    this.createCharCompanie(data["sum_cancelled"],text,this.CompanieForm.value["typeGraph"])
+  }
+
+  async  openGraphMeanDelayState(content: any) {
+    //étape 1
+    let data = await this.handleDelayStates();
+    //étape 2
+    this.openlg(content)
+    //étape 3
+    let text="Les retards selon l'état "+this.StatesForm.value["codeStateOrigin"]+ " à destination de " +this.StatesForm.value["codeStateDest"]  + " pour l\'année: "+this.StatesForm.value["year"]
+    this.createCharCompanie(data["mean_delay"],text,this.StatesForm.value["typeGraph"])
+  }
+
+  async openGraphMeanDelayCity(content: any) {
+    //étape 1
+    let data = await this.handleDelayCity();
+    //étape 2
+    this.openlg(content)
+    //étape 3
+    let text="Les retards selon la ville "+this.CityForm.value["codeCityOrigin"]+ " à destination de " +this.CityForm.value["codeCityDest"]  + " pour l\'année: "+this.CityForm.value["year"]
+    this.createCharCompanie(data["mean_delay"],text,this.CityForm.value["typeGraph"])
   }
 
   //Fonction pour créer le graph chart.js en param: le data
@@ -148,37 +251,9 @@ export class CompaniesComponent implements OnInit{
     });
   }
 
+  /*#########################################################################################
+#############################################################################################
+############################################################################################ */
 
-  async  openCancelledbyyCompanies(content: any) {
-    //étape 1
-    let data = await this.handleCancelledCompanies();
-    //étape 2
-    this.openlg(content)
-    //étape 3
-    let text="Vols annulés pour la companie "+this.CompanieForm.value["codeCompanie"] + " pour l\'anée: "+this.CompanieForm.value["year"]
-    this.createCharCompanie(data["sum_cancelled"],text,this.CompanieForm.value["typeGraph"])
-  }
-
-  async handleCancelledCompanies() : Promise<any>{
-    let paramGraph=this.CompanieForm.value;
-    let data = await this.companieService.CancelledCompanies(paramGraph["codeCompanie"],paramGraph["year"]).toPromise();
-    return data;
-  }
-
-  async  openGraphMeanDelayState(content: any) {
-    //étape 1
-    let data = await this.handleDelayStates();
-    //étape 2
-    this.openlg(content)
-    //étape 3
-    let text="retards selon l'état "+this.StatesForm.value["codeStateOrigin"] + " pour l\'anée: "+this.StatesForm.value["year"]
-    this.createCharCompanie(data["mean_delay"],text,this.StatesForm.value["typeGraph"])
-  }
-
-  async handleDelayStates() : Promise<any>{
-    let paramGraph=this.StatesForm.value;
-    let data = await this.companieService.delayStates(paramGraph["codeStateOrigin"],paramGraph["codeStateDest"],paramGraph["year"]).toPromise();
-    return data;
-  }
 
 }
