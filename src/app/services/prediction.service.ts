f6c162af209187472c294d7b94f357f59edaf0ca
nacer <41012118@parisnanterre.fr>
2023-05-30 15:23:26 +0200

    Version 1.1.0

---
+++ src/app/services/prediction.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PredictionService {
+
+  private readonly predictinBaseUrl = `${environment.baseUrl}`
+
+  constructor(private httpClient: HttpClient ) { }
+
+
+  public predictionFlight(paramGraphElement: object):Observable<any>{
+    let urlApi=this.predictinBaseUrl+'predict'
+    return this.httpClient.post(urlApi, paramGraphElement)
+
+    console.log(urlApi)
+    return this.httpClient.get(urlApi)
+  }
+}
